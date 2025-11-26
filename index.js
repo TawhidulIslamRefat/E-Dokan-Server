b56ef8fe32f8d1ee4caf4ba97016d172911a2f07
TawhidulIslamRefat <tirefat544@gamil.com>
2025-11-26 23:09:52 +0600

    Deploy Vercel and server complete

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("eDokan");
     const productCollection = db.collection("products");
 
@@ -78,7 +78,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
